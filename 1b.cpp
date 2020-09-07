ac37cf88cd6d07ea3499515f0fed3bb8e9a1aa87
Bendik <bendik98ny@gmail.com>
2020-09-07 11:02:24 +0200

    Things still not fixed. Trying to checkout last working state

--- 1b.cpp
+++ 1b.cpp
@@ -4,14 +4,14 @@
 #include <fstream>
 using namespace std;
 
-int wfile(int n, 	vector <double> x, 	vector <double> v)
+int wfile(int n, 	double *x, double *v)
 {
 	int pow = (int)log10(n);
 	string filename = "num_result_pow" + to_string(pow) + ".txt";
 	ofstream results;
 	results.open (filename);
 	results << "x           v" << endl;
-	for (int i = 0; i < x.size(); i++)
+	for (int i = 0; i < n+1; i++)
 	{
 		results << x[i] << " " << v[i] << endl;
 	}
@@ -21,54 +21,69 @@ int wfile(int n, 	vector <double> x, 	vector <double> v)
 
 int solve(int n)
 {
-	vector <double> x(n);
-	vector <double> a(n,-1);
-	vector <double> b(n,2);
-	vector <double> c(n,-1);
-	vector <double> d(n);
-	vector <double> v(n);
-	vector <double> b_tilde(n);
-	vector <double> c_tilde(n);
-	vector <double> d_tilde(n+1);
+	double *x = new double[n+1];
+	int *a = new int[n];
+	int *b = new int[n];
+	int *c = new int[n];
+	double *d = new double[n];
+	double *v = new double[n+1];
+	double *b_tilde = new double[n];
+	double *d_tilde = new double[n];
 
 	double h = 1/(double(n)+1);
 	double hh = 100*h*h;
 
-	for (int i=0; i<n; i++)
+	for (int i=0; i<n+1; i++)
 	{
 			x[i]=i*h;
 			d[i]=hh*exp(-10*x[i]);
 	}
 
+	for (int i = 0; i < n; i++)
+	{
+		a[i]=c[i]=-1;
+		b[i] = 2;
+	}
+
 	//Initial values
 	b_tilde[0] = b[0];
 	d_tilde[0] = d[0];
-	v[0]=0;
+	v[0] = v[n] = 0;
+
+
 
 	//Solving algorithm
 	for (int i = 1; i < n; i++)
 	{
 		b_tilde[i] = b[i] - a[i-1] * c[i-1] / b_tilde[i-1];
 		d_tilde[i] = d[i] - a[i-1] * d_tilde[i-1] / b_tilde[i-1];
-		c_tilde[i] = c[i]*a[i]/b[i];
-		v[i] = (d_tilde[i-1] - b_tilde[i-1] * v[i-1]) * b_tilde[i-1] / c[i-1];
 	}
 
-	v[n] = d_tilde[n]/b_tilde[n];
+	v[n-1] = d_tilde[n-1]/b_tilde[n-1];
 
-	for (int i = 1; i < n; i++)
+	for (int i = n-2; i > 0; i--)
 	{
-		v[n-i] = (d[n-i]-c_tilde[n-i]*v[n-i+1])/b_tilde[n-i];
+		v[i] = (d[i]- c[i]*v[i+1])/b_tilde[i];
 	}
 	//Write to file
 	wfile(n,x,v);
+
+	delete [] a;
+	delete [] b;
+	delete [] c;
+	delete [] d;
+	delete [] b_tilde;
+	delete [] d_tilde;
+	delete [] x;
+	delete [] v;
+
 	return 0;
 }
 
 
 int main(int argc, char* argv[])
 {
-	for (int i=1; i<3; i++)
+	for (int i=1; i<5; i++)
 	{
 		solve(pow(10,i));
 	}
